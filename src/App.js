cb5e28cd396cd2a00704bd0a382e2277eb20f647
korex71 <33636512+korex71@users.noreply.github.com>
2020-07-03 03:32:43 -0300

    Added bootstrap to react page frontend

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {BrowserRouter, Switch, Route, Link} from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <BrowserRouter>
+            <div>
+                <nav>
+                    <ul>
+                        <li><Link to="/sign-in">Sign-in</Link></li>
+                        <li><Link to="/sign-in">Sign-in</Link></li>
+                        <li><Link to="/manage/links/create">Create link</Link></li>
+                        <li><Link to="/manage/links/edit">Edit link</Link></li>
+                        <li><Link to="/manage/links">Links</Link></li>
+                    </ul>
+                </nav>
+                <Switch>
+                    <Route path="/sign-in"><h1>Sign in</h1></Route>
+                    <Route path="/sign-up"><h1>Sign up</h1></Route>
+                    <Route path="/manage/links/create"><h1>Create link</h1></Route>
+                    <Route path="/manage/links/edit"><h1>Edit link</h1></Route>
+                    <Route path="/manage/links"><h1>Links</h1></Route>
+                    <Route path="/"><h1>Home</h1></Route>
+                </Switch>
+            </div>
+        </BrowserRouter>
+    )
 }
 
-export default App;
+export default App
